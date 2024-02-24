346a3fc5bc2b43f2928cfece92c78a58d2c62e60
Natnael Teklemariam <131717014+Natnae-l@users.noreply.github.com>
2024-02-24 10:15:39 +0300

    mongoDB configured

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const CORS = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+// connect dotenv file
+require("dotenv").config();
 
+// req parser middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// middlewares
+app.use(CORS());
+
+// connect databse and start the server
+
+mongoose
+  .connect(process.env.mongoDB)
+  .then(() =>
+    app.listen(
+      process.env.PORT,
+      console.log("app listening on port " + process.env.PORT)
+    )
+  )
+  .catch((err) => console.log(err.message));
 
 app.get("/", (req, res, next) => {
   res.send("HI");
 });
-
-app.listen(3000, console.log("app listening on port 3000"));
