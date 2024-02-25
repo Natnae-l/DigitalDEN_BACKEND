dcf181ff139c4cdb90f0552dee348fa5429ff0f7
Natnael Teklemariam <131717014+Natnae-l@users.noreply.github.com>
2024-02-25 10:35:05 +0300

    coludinary image upload cloud storage added

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,15 @@
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const Item = require("../model/itemModel");
 const multer = require("multer");
-const path = require("path");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 const router = express.Router();
 
@@ -21,7 +29,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.get("/upload", async (req, res, next) => {
-  res.sendFile(path.join(__dirname, "n.html"));
+  res.sendFile(path.join(__dirname, "../n.html"));
 });
 
 router.get("/", async (req, res, next) => {
@@ -29,12 +37,24 @@ router.get("/", async (req, res, next) => {
     let createdItem = await Item.find();
     res.json(createdItem);
   } catch (error) {
-    console.log(error.message);
+    next(error);
   }
 });
 router.post("/", upload.single("image"), async (req, res, next) => {
-  console.log(req.file);
+  const { filename, destination } = req.file;
+
   try {
+    let result = await cloudinary.uploader.upload(
+      req.file.destination + req.file.filename
+    );
+    console.log(result);
+    fs.unlink(path.join(__dirname, "../" + destination + filename), (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Deleted");
+      }
+    });
+
     let createdItem = await Item.create(req.body);
     res.json(createdItem);
   } catch (error) {
